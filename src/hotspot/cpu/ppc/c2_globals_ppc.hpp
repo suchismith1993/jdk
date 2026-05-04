850ba9c67abb07bc07d61b927450f948505beff3
suchismith1993 <suchismith1993@gmail.com>
2026-05-04 10:53:08 -0500

    extra lines

--- src/hotspot/cpu/ppc/c2_globals_ppc.hpp
+++ src/hotspot/cpu/ppc/c2_globals_ppc.hpp
@@ -95,4 +95,6 @@ define_pd_global(bool,     TrapBasedRangeChecks,          true);
 define_pd_global(bool,     NeverActAsServerClassMachine, false);
 
 define_pd_global(size_t, ConstantTableSizeThreshold, 32 * 1024);
+
 #endif // CPU_PPC_C2_GLOBALS_PPC_HPP
+
